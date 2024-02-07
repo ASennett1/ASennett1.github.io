962729ce05d10be9e876b384adf288ef9ebf16a3
Andy Sennett <700172167@fvtc.edu>
2024-02-06 19:53:34 -0600

    Added welcome div

--- site.js
+++ site.js
@@ -1,1 +1,27 @@
-console.log('Hello World')
+
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+
+const message =  {
+isMorning: "Good morning",
+isAfternoon: "Good Afternoon",
+isEvening: "Good Evening"
+}
+
+let dynamicMessage;
+
+if (isMorning) {
+    dynamicMessage = message.isMorning;
+}
+else if (isAfternoon){
+    dynamicMessage = message.isAfternoon;
+}
+else if (isEvening) {
+    dynamicMessage = message.isEvening;
+}
+
+document.getElementById("welcome").innerHTML = dynamicMessage;
