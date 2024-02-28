d202bd585a58525b76ba480a7a1ff3c210741bc4
Andy Sennett <700172167@fvtc.edu>
2024-02-27 21:58:48 -0600

    Added secret message

--- site.js
+++ site.js
@@ -1,27 +1,3 @@
+const message = `It's dangerous to go alone! Take this. -]===>`
 
-const hours = new Date().getHours() // get the current hour
-
-const isMorning = hours >= 4 && hours < 12 // is it morning?
-const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
-const isEvening = hours >= 17 || hours < 4 // is it evening?
-
-
-const message =  {
-isMorning: "Good morning",
-isAfternoon: "Good Afternoon",
-isEvening: "Good Evening"
-}
-
-let dynamicMessage;
-
-if (isMorning) {
-    dynamicMessage = message.isMorning;
-}
-else if (isAfternoon){
-    dynamicMessage = message.isAfternoon;
-}
-else if (isEvening) {
-    dynamicMessage = message.isEvening;
-}
-
-document.getElementById("welcome").innerHTML = dynamicMessage;
+localStorage.setItem(`It's a secret to everybody.`, message) 
